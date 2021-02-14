25cf71ba50d684e685cf057f7f6154fa7650eec1
Dylan773 <73709388+Dylan773@users.noreply.github.com>
2021-02-14 23:16:58 +0000

    Created test methods for tstStaff and properties for clsStaff.
    Populated tstStaff with test methods for each property in tblStaff.
    Populated clsStaff with properties from tblStaff.

--- ClassLibrary/clsStaff.cs
+++ ClassLibrary/clsStaff.cs
@@ -1,6 +1,14 @@
-﻿namespace ClassLibrary
+﻿using System;
+
+namespace ClassLibrary
 {
     public class clsStaff
     {
+        public bool IsManager { get; set; }
+        public DateTime DOB { get; set; }
+        public int StaffID { get; set; }
+        public string StaffPassword { get; set; }
+        public string FirstName { get; set; }
+        public string FamilyName { get; set; }
     }
 }
