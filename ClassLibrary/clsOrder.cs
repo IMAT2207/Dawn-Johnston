a1fb928304df0bf25f77fab1a5b48957c31a739c
Jordan T Gray <Business@shinkson47.in>
2021-02-14 22:35:41 +0000

    Created basic Order classes

---
+++ ClassLibrary/clsOrder.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassLibrary
+{
+    class clsOrder
+    {
+        int OrderID;
+        string OrderState;
+        static readonly string[] ORDER_STATES = new string[] { "WIP", "Finalized", "Shipped", "Delivered" };
+        int ProcessedBy;
+        int OrderedBy;
+        DateTime PlacedOn;
+        string DelieveryNote;
+        Boolean PaidFor;
+    }
+
+    class clsOrderItem
+    {
+        string OrderItemID;
+        int OrderID;
+        int ProductID;
+    }
+}
