03cf13023b57236642ce0043746adef74fd77f92
fairywizard15 <mankapui15@yahoo.com.hk>
2021-02-15 01:10:36 +0000

    tst.Stock.cs complete

---
+++ ClassLibrary/clsStock.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ClassLibrary;
+
+namespace ClassLibrary
+{
+    public class clsStock
+    {
+        public clsStock AStock = new clsStock();
+        public int ProductId;
+        public string ProductName;
+        public string ProductDescription;
+        public Boolean IsAvailable = false;
+        public int QuantityAvailable = 0;
+        public DateTime RestockDate;
+
+        public bool Find(int ProductId)
+        {
+            ProductId = 1;
+            ProductName = "Beef";
+            ProductDescription = "About Something...";
+            IsAvailable = true;
+            QuantityAvailable = 10;
+            RestockDate = DateTime.Now;
+
+            return true;
+        }
+
+    }
+}
