806eae6d2365b88c212ca1100678646e73bcf0a6
Jordan T Gray <Business@shinkson47.in>
2021-02-17 23:42:31 +0000

    Made a data entry form that isn't 100% aids
    Also documented clsOrder and mutated the string states to an enum. I'll deal with it later.

--- AdminSystem/OrderDataEntry.aspx.cs
+++ AdminSystem/OrderDataEntry.aspx.cs
@@ -19,25 +19,30 @@ public partial class OrderDataEntry : System.Web.UI.Page
 
         order.OrderID = int.Parse(txtID.Text);
 
-        order.OrderState = cmbState.SelectedValue;
+        //order.State = cmbState.SelectedValue;
 
-        order.ProcessedBy = int.Parse(txtProcessedBy.Text);
+        order.ProcessedBy = int.Parse(txtProc.Text);
 
         // STOPSHIP
         order.OrderedBy = 1; // TODO change to currently logged in customer PK.
 
         order.PlacedOn = DateTime.Now;
 
-        order.DeliveryNote = txtDeliveryNote.Text;
+        order.DeliveryNote = txtDelivery.Text;
 
         order.PaidFor = true;
 
         Session["Order"] = order;
-        lblError.Text = "Success!";
+        lblMsg.Text = "Success!";
         Response.Redirect("OrderViewer.aspx");
     }
 
     protected void btnCancel_Click(object sender, EventArgs e)
+    {
+
+    }
+
+    protected void btnFind_Click(object sender, EventArgs e)
     {
 
     }
--- ClassLibrary/clsOrder.cs
+++ ClassLibrary/clsOrder.cs
@@ -6,23 +6,77 @@ using System.Threading.Tasks;
 
 namespace ClassLibrary
 {
+
+    /// <summary>
+    /// A mutable, managed and validated representation of a database Order record.  
+    /// </summary>
     public class clsOrder
     {
-        public int OrderID;
-        public string OrderState;
-        public static readonly string[] ORDER_STATES = new string[] { "WIP", "Finalized", "Shipped", "Delivered" };
-        public int ProcessedBy;
-        public int OrderedBy;
-        public DateTime PlacedOn;
-        public string DeliveryNote;
-        public Boolean PaidFor;
+        #region constants
+        /// <summary>
+        /// Valid values for <a cref="OrderState"/>.
+        /// </summary>
+        public enum OrderState { WIP, Finalized, Shipped, Delivered };
+        #endregion
 
+
+        #region attributes
+        /// <summary>
+        /// The primary key of this order.
+        /// </br></br>
+        /// Cannot be null. If no order ID is found or assigned, will be -1/
+        /// </summary>
+        public int OrderID = -1;
+
+        /// <summary>
+        /// An enum of <a cref="OrderState"/> which represents the current state of the order.
+        /// <br/><br/>
+        /// By default is WIP. May not be null.
+        /// </summary>
+        public OrderState State = OrderState.WIP;
+
+        /// <summary>
+        /// The primary key of a the staff member who processed this order.
+        /// </summary>
+        public int ProcessedBy;
+
+        /// <summary>
+        /// The primary key of the patron who placed this order
+        /// </summary>
+        public int OrderedBy;
+
+        /// <summary>
+        /// The epoch on which this order was placed
+        /// </summary>
+        public DateTime PlacedOn;
+
+        /// <summary>
+        /// Any delivery note left by the user upon placing the order.
+        /// </summary>
+        public string DeliveryNote;
+
+        /// <summary>
+        /// 
+        /// </summary>
+        public Boolean PaidFor;
+        #endregion attributes
+
+        #warning for test data only, orders should not assume data; it may be treated as a valid record.
         public clsOrder() => Find(1);
 
+        /// <summary>
+        /// Replaces which record this instance represents.
+        /// <br/><br/>
+        /// Calling this with a valid Order Primary Key mutates this order to represent that record from the database.
+        /// </summary>
+        /// <param name="ID">A valid Order ID primary key.</param>
+        /// <returns>True if ID was valid, record was found, and order was successfully mutated.</returns>
         public bool Find(int ID)
         {
-            OrderID = 1;
-            OrderState = "WIP";
+            if (!IDIsValid(ID)) return false;
+
+            OrderID = ID;
+            State = OrderState.WIP;
             ProcessedBy = 100;
             OrderedBy = 420;
             PlacedOn = DateTime.Now;
@@ -30,6 +84,14 @@ namespace ClassLibrary
             PaidFor = true;
             return true;
         }
+
+        #warning TODO this should also check if the ID exists on the database.
+        /// <summary>
+        /// Determines if the parsed ID is a valid order primary key, and a corresponding record exists on the database.
+        /// </summary>
+        /// <param name="ID"></param>
+        /// <returns>True if the Id is valid, else false.</returns>
+        public static bool IDIsValid(int ID) => ID > 0;
     }
 
     public class clsOrderItem
