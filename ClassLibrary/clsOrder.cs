3b88a4a600cda99d80b35fbe6224b9ef7988bdd0
Jordan T Gray <Business@shinkson47.in>
2021-03-04 19:48:10 +0000

    Partial completion of testing

--- ClassLibrary/clsOrder.cs
+++ ClassLibrary/clsOrder.cs
@@ -101,7 +101,7 @@ namespace ClassLibrary
 
         public bool SetOrderedBy(int CustomerID)
         {
-            if (IntAttributeValid(CustomerID, clsCustomer.CUST_ID_MAX) && new clsCustomer().Find(CustomerID)) // If record with that ID already exists, ignore.
+            if (IntAttributeValid(CustomerID, int.MaxValue) && new clsCustomer().Find(CustomerID)) // If record with that ID already exists, ignore.
             {
                 OrderedBy = CustomerID;
                 return true;
@@ -134,7 +134,11 @@ namespace ClassLibrary
                 DeliveryNote = note;
                 return true;
             } else
+            {
+                DeliveryNote = "";
                 return false;
+            }
+
         }
 
         /// <summary>
@@ -221,24 +225,26 @@ namespace ClassLibrary
             return true;
         }
 
-        /// <summary>
-        /// Determines if the parsed ID is a valid order primary key, and a corresponding record exists on the database.
-        /// <br/><br/>
-        /// If the ID is positive, checks the database for any matching entries.
-        /// Returns false if ID is negative, or the database returned no results.
-        /// <br/><br/>
-        /// SIDE EFFECT: Configures <a cref="clsOrder#bd"/>, which will contain matching records.
-        /// </summary>
-        /// <param name="ID"></param>
-        /// <returns>True if the Id is valid, else false.</returns>
-        public static bool IDIsValid(int ID, clsOrder order) {
-            if (ID < 0) return false;                       // Don't fetch if id is low
-            order.db.SQLParams.Clear();
-            order.db.AddParameter("@OrderID", ID);          // Fetch records
-            order.db.Execute(SPROC);
-            return order.db.Count > 0;                      // Return true if there was matches.
-        }
+        public string Valid() => Valid(this);
 
+        public static string Valid(clsOrder order)
+        {
+            string message = "";
+
+            if (order.DeliveryNote.Length > DELIVERY_NOTE_LENGTH)
+                message += "Delivery note is too long!";
+
+
+
+
+
+
+
+
+
+
+            return message;
+        }
 
     }
 
