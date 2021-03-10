8052f2cc351260ed75ca56d668b14396d2bce08e
fairywizard15 <mankapui15@yahoo.com.hk>
2021-03-10 20:52:38 +0000

    week22 awaiting for vm DB

---
+++ ClassLibrary/clsStockCollection.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ClassLibrary
+{
+    public class clsStockCollection
+    {
+        //private data member for the list
+        List<clsStock> mStockList = new List<clsStock>();
+        //public list for StockList
+        public List<clsStock> StockList
+        {
+            get
+            {
+                //return the private data
+                return mStockList;
+            }
+            set
+            {
+                //set the private data
+                mStockList = value;
+            }
+        }
+        //public property for count
+        public int Count
+        {
+            get
+            {
+                //return the count of the list
+                return mStockList.Count;
+            }
+            set
+            {
+
+            }
+        }
+        //public property for ThisStock
+        public clsStock ThisStock { get; set; }
+
+        //constructor for the class
+        public clsStockCollection()
+        {
+            //var for the index
+            Int32 Index = 0;
+            //var to store the record count
+            Int32 RecordCount = 0;
+            //object for data connection
+            clsDataConnection DB = new clsDataConnection();
+            //execute the stored procedure
+            DB.Execute("");
+            //create the items of test data
+            clsStock TestItem = new clsStock();
+            //set its properties
+            TestItem.ProductId = 1;
+            TestItem.ProductName = "Beef";
+            TestItem.ProductDescription = "Something Good";
+            TestItem.IsAvailable = true;
+            TestItem.QuantityAvailable = 1;
+            TestItem.RestockDate = DateTime.Now.Date;
+            //add the item to the test list
+            mStockList.Add(TestItem);
+            //re initialise the obkect for some new data
+            TestItem = new clsStock();
+            //set its properties
+            TestItem.ProductId = 2;
+            TestItem.ProductName = "Chicken";
+            TestItem.ProductDescription = "Something Better";
+            TestItem.IsAvailable = true;
+            TestItem.QuantityAvailable = 1;
+            TestItem.RestockDate = DateTime.Now.Date;
+            //add the item to the test list
+            mStockList.Add(TestItem);
+        }
+    }
+}
---
+++ TestingStock/tstStockCollection.cs
@@ -0,0 +1,113 @@
+﻿using System;
+using ClassLibrary;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Collections.Generic;
+
+
+namespace TestingStock
+{
+    [TestClass]
+    public class tstStockCollection
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+
+        }
+
+        [TestMethod]
+        public void InstanceOK()
+        {
+            //create an instance of the class
+            clsStockCollection AllStocks = new clsStockCollection();
+            //test it exists
+            Assert.IsNotNull(AllStocks);
+        }
+
+        [TestMethod]
+        public void StockListOK()
+        {
+            //create an instance
+            clsStockCollection AllStocks = new clsStockCollection();
+            //create data needs to be a list of oject
+            List<clsStock> TestList = new List<clsStock>();
+            //create the item f test data
+            clsStock TestItem = new clsStock();
+            //set its properties
+            TestItem.ProductId = 1;
+            TestItem.ProductName = "Beef";
+            TestItem.ProductDescription = "Something Good";
+            TestItem.IsAvailable = true;
+            TestItem.QuantityAvailable = 1;
+            TestItem.RestockDate = DateTime.Now.Date;
+            //add the item to the test list
+            TestList.Add(TestItem);
+            //assign the data to the property
+            AllStocks.StockList = TestList;
+            //test to see that the two values are the same
+            Assert.AreEqual(AllStocks.StockList, TestList);
+        }
+
+        [TestMethod]
+        public void CountProductOK()
+        {
+            //create an instance of the class
+            clsStockCollection AllStocks = new clsStockCollection();
+            //create some test data to assign to the property
+            Int32 SomeCount = 2;
+            //assign the data to the property
+            AllStocks.Count = SomeCount;
+            Assert.AreEqual(AllStocks.Count, SomeCount);
+        }
+
+        [TestMethod]
+        public void ThisStockOK()
+        {
+            //create an instance of the class
+            clsStockCollection AllStocks = new clsStockCollection();
+            //create some test data to assign
+            clsStock TestStock = new clsStock();
+            //set the properties of the test object
+            TestStock.ProductId = 1;
+            TestStock.ProductName = "Beef";
+            TestStock.ProductDescription = "Something Good";
+            TestStock.IsAvailable = true;
+            TestStock.QuantityAvailable = 1;
+            TestStock.RestockDate = DateTime.Now.Date;
+            //assign data
+            AllStocks.ThisStock = TestStock;
+            Assert.AreEqual(AllStocks.ThisStock, TestStock);
+        }
+
+        [TestMethod]
+        public void ListAndCountOK()
+        {
+            //create an instance of the class
+            clsStockCollection AllStocks = new clsStockCollection();
+            //create some test data to the list object
+            List<clsStock> TestList = new List<clsStock>();
+            //create the item of test data
+            clsStock TestItem = new clsStock();
+            //set its properties
+            TestItem.ProductId = 1;
+            TestItem.ProductName = "Beef";
+            TestItem.ProductDescription = "Something Good";
+            TestItem.IsAvailable = true;
+            TestItem.QuantityAvailable = 1;
+            TestItem.RestockDate = DateTime.Now.Date;
+            //add the item to the test list
+            TestList.Add(TestItem);
+            //assign the data
+            AllStocks.StockList = TestList;
+            Assert.AreEqual(AllStocks.Count, TestList.Count);
+        }
+
+        [TestMethod]
+        public void TwoRecordsPresent()
+        {
+            //create an instance of the class
+            clsStockCollection AllStocks = new clsStockCollection();
+            Assert.AreEqual(AllStocks.Count, 2);
+        }
+    }
+}
