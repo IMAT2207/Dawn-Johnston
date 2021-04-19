38e09e967ad32e93c5753abd6d55de54cd8f7bf0
shinkson47 <business@shinkson47.in>
2021-04-19 12:20:38 +0100

    Order Viewer QOL

--- AdminSystem/OrderViewer.aspx.cs
+++ AdminSystem/OrderViewer.aspx.cs
@@ -11,12 +11,18 @@ public partial class OrderViewer : System.Web.UI.Page
     protected void Page_Load(object sender, EventArgs e)
     {
         clsOrder clsOrder = (clsOrder) Session["order"];
-        Response.Write(clsOrder.OrderID);
-        Response.Write(clsOrder.ProcessedBy);
-        Response.Write(clsOrder.OrderedBy);
-        Response.Write(clsOrder.State);
-        Response.Write(clsOrder.PlacedOn);
-        Response.Write(clsOrder.DeliveryNote);
-        Response.Write(clsOrder.PaidFor);
+        if (clsOrder == null) Button1_Click(null, null);
+        Response.Write(clsOrder.OrderID + "<br/>");
+        Response.Write(clsOrder.ProcessedBy + "<br/>");
+        Response.Write(clsOrder.OrderedBy + "<br/>");
+        Response.Write(clsOrder.State + "<br/>");
+        Response.Write(clsOrder.PlacedOn + "<br/>");
+        Response.Write(clsOrder.DeliveryNote + "<br/>");
+        Response.Write(clsOrder.PaidFor + "<br/>");
+    }
+
+    protected void Button1_Click(object sender, EventArgs e)
+    {
+        Response.Redirect("OrderList.aspx");
     }
 }
