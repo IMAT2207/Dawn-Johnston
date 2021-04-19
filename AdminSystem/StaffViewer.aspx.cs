3510c67de0f4e8140ba9dd66db39c47f906ee6c5
Dylan <dylanbrand773@gmail.com>
2021-04-19 10:33:38 +0100

    Minor tweaks
    Updated the UI on StaffViewer

--- AdminSystem/StaffViewer.aspx.cs
+++ AdminSystem/StaffViewer.aspx.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
+using ClassLibrary;
 
 public partial class StaffViewer : System.Web.UI.Page
 {
@@ -13,14 +14,15 @@ public partial class StaffViewer : System.Web.UI.Page
 
         StaffMember = (clsStaff)Session["StaffMember"];
 
-        Response.Write(StaffMember.StaffPassword);
-        Response.Write(StaffMember.RecordCreated);
-        Response.Write(StaffMember.FirstName);
-        Response.Write(StaffMember.FamilyName);
+        Response.Write("First name: " + StaffMember.FirstName + "<br/>");
+        Response.Write("Family name: " + StaffMember.FamilyName + "<br/>");
+        Response.Write("Password: " + StaffMember.StaffPassword + "<br/>");
+        Response.Write("Record Created: " + StaffMember.RecordCreated + "<br/>");
+        
     }
 
     protected void btnStaffList_Click(object sender, EventArgs e)
     {
-
+        Response.Redirect("StaffList.aspx");
     }
 }
